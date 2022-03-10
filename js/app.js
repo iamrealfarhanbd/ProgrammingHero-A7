081208868ef0475a3be6ad2cc01f615aa8b779b8
unknown <iamfarhan09@gmail.com>
2022-03-10 21:35:49 +0600

    only reported post show  Fixed

--- js/app.js
+++ js/app.js
@@ -153,7 +153,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
